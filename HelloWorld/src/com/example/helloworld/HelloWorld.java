113cb18b267362b53f731ed5a22ca2e0a10e7e95
Chunmeista <barryn.chun@gmail.com>
2015-08-27 10:26:18 -1000

    initial sync

---
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+/**
+ * Created by Chunmeista on 8/25/2015.
+ */
+public class HelloWorld {
+
+    public static void main (String [] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
