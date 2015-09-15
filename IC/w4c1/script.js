e187f9d02002397f3551b9961a3a08241528c38e
Chunmeista <barryn.chun@gmail.com>
2015-09-15 11:15:57 -1000

    w4c1

---
+++ IC/w4c1/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Chunmeista on 9/15/2015.
+ */
+
+function mouseOver(div) {
+    div.style.backgroundColor = "#4d9d67";
+    div.style.color = "white";
+}
+
+function mouseOut(div) {
+    div.style.backgroundColor = "";
+    div.style.color = "";
+}
